3d6d7488e48c1694858e5c0323e2b3258ce14852
m-henmi <m.henmi@gmail.com>
2015-09-28 08:20:33 +0900

    JUnit を 3 から 4 に変更

--- sapmle01/src/main/java/org/henmix/sapmle01/App.java
+++ sapmle01/src/main/java/org/henmix/sapmle01/App.java
@@ -4,10 +4,9 @@ package org.henmix.sapmle01;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public String hello(String name) {
+        return "Hello, " + name + ".";
     }
 }
--- sapmle01/src/test/java/org/henmix/sapmle01/AppTest.java
+++ sapmle01/src/test/java/org/henmix/sapmle01/AppTest.java
@@ -1,38 +1,18 @@
 package org.henmix.sapmle01;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
+	@Test
+	public void helloTest() {
+		App app = new App();
+		String actual = app.hello("hoge");
+		String expected = "Hello, hoge.";
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+		assertThat(actual, is(expected));
+	}
+
 }
